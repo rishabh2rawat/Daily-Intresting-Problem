7b92fe1d52ede10f9b9314c4a28a555c17b984a2
Rishabh Rawat <rishabhricharawat@gmail.com>
2020-04-21 01:30:20 +0530

    Add files via upload
    adding the Souse code and the question file 
    q1. you are given random no from 0--1 at an n instance ...... and using that you have to find the value of pi

---
+++ Findpi.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Findpi
+{
+    public static void main(String[] args) {
+        int pointic=0;                              //points inside circle 
+        
+        Scanner scan=new Scanner(System.in); 
+        System.out.println("ENTER THE NO OF INTANCE");
+        long n=scan.nextLong();                       //no of random points 0....1
+        long z=n;
+        scan.close();
+
+        float x_i=0;                                //x axis point for random variable 
+        float x_y=0;                                //y axis point for random variable 
+        
+        
+        while(n>0)
+        {
+            Random rand =new Random();
+            x_i=rand.nextFloat();
+            x_y=rand.nextFloat();
+            
+            float dis = ((x_i * x_i) + (x_y * x_y));
+            if(dis<=1)
+            {
+                pointic++;
+            }
+            n--;
+        }
+
+        pointic*=4;
+        double pi= (double)pointic/z;
+        
+        System.out.println(pi);
+
+
+
+    }
+}
